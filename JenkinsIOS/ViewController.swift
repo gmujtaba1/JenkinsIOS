0f74a228968906acd04545a7512347f82f286de6
Ghulam Mujtaba <ghulam.mujtaba@systemsltd.com>
2021-11-10 13:56:15 +0500

    Added test screen

--- JenkinsIOS/ViewController.swift
+++ JenkinsIOS/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var jenkinSwitch: UISwitch!
+    @IBOutlet weak var lblJenkinStatus: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        updateJenkinStatus(enabled: jenkinSwitch.isOn)
     }
 
-
+    @IBAction func jenkinSwitchAction(_ sender: Any) {
+        guard let state = sender as? UISwitch else {return}
+        updateJenkinStatus(enabled: state.isOn)
+    }
+    
+    private func updateJenkinStatus(enabled: Bool) {
+        lblJenkinStatus.text = enabled ? "Enabled" : "Disabled"
+    }
 }
 
