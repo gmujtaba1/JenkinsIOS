347683a8625e1f2b85841f3ccb72e4a7b46150d8
Ghulam Mujtaba <ghulam.mujtaba@systemsltd.com>
2021-11-11 15:34:07 +0500

    Added test cases

--- JenkinsIOSTests/JenkinsIOSTests.swift
+++ JenkinsIOSTests/JenkinsIOSTests.swift
@@ -10,24 +10,37 @@ import XCTest
 
 class JenkinsIOSTests: XCTestCase {
 
+    var vc: ViewController!
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        try super.setUpWithError()
+        vc = UIStoryboard(name: "Main", bundle: nil).instantiateInitialViewController() as? ViewController
+        vc.loadViewIfNeeded()
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        
+        vc = nil
+        try super.tearDownWithError()
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testInitilization() throws {
+        XCTAssertNotNil(vc, "Failed to initilize view controller")
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testJenkinsOutletConnections() throws {
+        XCTAssertNotNil(vc.jenkinSwitch, "Jenkins status switch not connected")
+        XCTAssertNotNil(vc.lblJenkinStatus, "Jenkins status label is not connected")
     }
-
+    
+    func testJenkinsStatus() throws {
+        let status = vc.jenkinSwitch.isOn ? "Enabled" : "Disabled"
+        let statusDisplayValue = vc.lblJenkinStatus.text ?? ""
+        
+        XCTAssertEqual(statusDisplayValue, status, "Status doesn't !atch selection value")
+    }
+    
 }
